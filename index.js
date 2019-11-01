bc320a32286fc286cdf64f8eec1c8b72174838e8
Nishant <ni14@duke.edu>
2019-11-01 14:56:07 -0400

    finishing touches

--- index.js
+++ index.js
@@ -323,7 +323,7 @@ function decider(jsondata, userid, channelid) {
       case "introduction":
         pretext = "";
         text =
-          "Hi! I'm KitKat! I'm here to answer questions you may have! (Meals, submissions, sponsors, and more!) \n Try 'add [song name] by [artist name]' or 'I need help with [computer language or technical issue]'";
+          "Hi! I'm KitKat! I'm here to answer questions you may have! (Meals, submissions, sponsors, coding help, and more!)";
         sendMessage(pretext, text, color, channelid);
         break;
       default:
